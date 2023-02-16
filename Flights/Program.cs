0d428ae9f337a0abe3efbc13ca196d4df4277cef
Sam <s.zamb@t-online.de>
2023-02-16 20:35:31 +0100

    Implement OpenAPI, search daten Ausgetauscht (API), ASP.NET methoden namen für Angular angepasst

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -12,10 +12,14 @@ builder.Services.AddSwaggerGen(c =>
         Description = "Development Server",
         Url = "https://localhost:7201"
     });
+
+    c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}" +
+    $"{e.ActionDescriptor.RouteValues["controller"]}");
 });
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.WithOrigins("*"));
 app.UseSwagger().UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
